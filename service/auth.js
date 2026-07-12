77cb5ee8df99df853949e6e5507bdfff8dfe8c84
Guddu Kumar <142442965+gudduKumar548@users.noreply.github.com>
2026-07-12 23:55:02 +0530

    Refactor auth.js to remove unnecessary codes

--- service/auth.js
+++ service/auth.js
@@ -1,10 +1,8 @@
 const sessionIdToUserMap = new Map();
-
 function setUser(id, user) {
     sessionIdToUserMap.set(id, user);
 }
 function getUser(id, user) {
     sessionIdToUserMap.get(id, user);
 }
-
 module.exports = { setUser, getUser };
